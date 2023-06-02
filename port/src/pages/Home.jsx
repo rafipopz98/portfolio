72bdb8e6b74a1573ea067731743c3e23e85f9782
rafipopz98 <itsrealrafi@gmail.com>
2023-06-03 04:26:06 +0530

    About-last-Section

--- port/src/pages/Home.jsx
+++ port/src/pages/Home.jsx
@@ -1,13 +1,20 @@
-import React from "react";
-import HeroSection from "../components/HeroSection";
-import AboutSection from "../components/AboutSection";
-function Home() {
+import React from 'react';
+import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
+// import ProjectsSection from '../components/ProjectsSection';
+// import TestimonialsSection from '../components/TestimonialsSection';
+// import ContactBanner from '../components/ContactBanner';
+import HeroSection from '../components/HeroSection';
+
+export default function Home() {
   return (
     <div>
       <HeroSection />
       <AboutSection />
+      <ServicesSection />
+      {/* <ProjectsSection /> */}
+      {/* <TestimonialsSection />
+      <ContactBanner /> */}
     </div>
   );
 }
-
-export default Home;
