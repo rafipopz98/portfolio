c3b92ac345753f1f64b3e4da551a2e34b97b9197
rafipopz98 <itsrealrafi@gmail.com>
2023-06-03 01:22:21 +0530

    navbar

--- port/src/main.jsx
+++ port/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import GlobalStyles from './styles/GlobalStyle';
+import Typography from './styles/Typography';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+  <GlobalStyles />
+    <Typography />
     <App />
   </React.StrictMode>,
 )
