ec0fdc31a3fc0db70a6a022065f080c968b3b76e
rafipopz98 <itsrealrafi@gmail.com>
2023-06-03 16:23:03 +0530

    scrollbar

--- port/src/App.jsx
+++ port/src/App.jsx
@@ -5,11 +5,13 @@ import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
 import Footer from "./components/Footer";
+import ScrollToTop from "./components/SmoothScrollbar";
 function App() {
   return (
     <>
       <Router>
       <Navbar />
+      <ScrollToTop />
         <Routes>
             <Route exact path="/about" element={<About />} > </Route>
             <Route exact path="/projects" element={<Project />} > </Route>
--- port/src/components/SmoothScrollbar.jsx
+++ port/src/components/SmoothScrollbar.jsx
@@ -1,12 +1,43 @@
+import Scrollbar from 'smooth-scrollbar';
 import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import OverscrollPlugin from 'smooth-scrollbar/plugins/overscroll';
 
-export default function ScrollToTop() {
-  const { pathname } = useLocation();
+const overscrollOptions = {
+  enable: true,
+  effect: 'bounce',
+  damping: 0.15,
+  maxOverscroll: 150,
+  glowColor: '#fff',
+};
+
+//   const overscrollOptions = {
+//     enable: true,
+//     effect: 'glow',
+//     damping: 0.1,
+//     maxOverscroll: 200,
+//     glowColor: '#222a2d',
+//   };
+
+const options = {
+  damping : 0.07,
+  plugins: {
+
+    overscroll: { ...overscrollOptions },
+
+  },
+}
+
+const Scroll = () => {
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
 
-  return null;
+    Scrollbar.use(OverscrollPlugin);
+    Scrollbar.init(document.body, options);
+
+    return () => {
+      if (Scrollbar) Scrollbar.destroy(document.body)
+    }  },[])
+return null;
 }
+
+export default Scroll;
